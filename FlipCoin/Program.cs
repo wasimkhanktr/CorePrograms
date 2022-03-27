4c8075f253b181addad3317fd5d1d277c3860149
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-27 16:44:40 +0530

    [Wasim] Add: Find Even and Odd Number

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a no");
+            int no = Convert.ToInt32(Console.ReadLine());
+            if (no % 2 == 0)
+                Console.WriteLine("Even No");
+            else
+                Console.WriteLine("Odd No");
+        }
+    }
+}
